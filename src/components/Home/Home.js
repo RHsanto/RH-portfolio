cab71e0c9c77d1bd63613457b2f451d1ab571d68
RHsanto <rakibulsanto789@gmail.com>
2022-04-05 16:00:02 +0600

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Blog from '../Blog/Blog';
-import Contact from '../Contact/Contact';
 import Projects from '../Projects/Projects';
 import About from './About/About';
 import Banner from './Banner';
@@ -12,7 +12,21 @@ const Home = () => {
       <About/>
       <Projects/>
       <Blog/>
-      <Contact/>
+      {/* contact section */}
+      <div className="container">
+        <div className="row my-5">
+          <div className="col-lg-6 mx-auto contact text-center">
+          <h1>Get In Touch</h1>
+          <p>I’m currently looking for new opportunities, my inbox is always open. Whether you have a 
+            question or just want to say hi, I’ll try my best to get back to you!</p>
+            <div className="contact-btn">
+             <Link to='/contact'>
+             Say Hello
+             </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,31 +1,107 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import project1 from '../../image/project-1.png'
 import './Projects.css'
+import { AiOutlineGithub } from "react-icons/ai";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { Link } from 'react-router-dom';
+
 const Projects = () => {
   return (
     <div className='project'>
      <div className="container">
-       <div className="row my-container">
+       <div className="row  my-container">
      <h3 className='title'>Projects</h3>
-         <div className="col-lg-6">
+     {/* project-1 */}
+        <div className="col-lg-12">
+          <div className="row div">
+          <div className="col-lg-6 ">
          <div className="projects mt-5">
            <img src={project1} alt="" />
          </div>
          </div>
-         <div className="col-lg-6 site">
-        <p>Bloging Site</p>
+         <div className="col-lg-6 site ">
+        <p>Blogging Site</p>
         <h3>Docy</h3>
-        <div className="info">
+        <div className="info ">
         A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, 
         Package Control, Atom Package Manager, and npm.
         </div>
-        <p className='mt-4'>VS Code
-         Sublime Text
-         Atom
-         iTerm2
-         Hyper</p>
+        <ul className='mt-4 d-flex justify-content-end tools'><li>VS Code</li>
+         <li> Sublime Text</li>
+         <li>Atom</li>
+         <li>iTerm2</li>
+         <li>Hyper</li></ul>
+         <div className="link-up">
+           <a target='_blank' href="https://github.com/thekawsarhossain/Docs-client-site"><AiOutlineGithub/></a>
+           <a target='_blank' href="https://docs-client-site.vercel.app/"><HiOutlineExternalLink/></a>
          </div>
+         </div>
+          </div>
+        </div>
+     {/* project-2 */}
+        <div className="col-lg-12">
+    <div className="row">
+    <div className="col-lg-6 site2 ">
+        <p>Blogging Site</p>
+        <h3>Docy</h3>
+        <div className="info2 ">
+        A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, 
+        Package Control, Atom Package Manager, and npm.
+        </div>
+        <ul className='mt-4 d-flex p-0 tools-2'>
+        <li>VS Code</li>
+         <li> Sublime Text</li>
+         <li>Atom</li>
+         <li>iTerm2</li>
+         <li>Hyper</li></ul>
+         <div className="link-up">
+           <a target='_blank' href="https://github.com/thekawsarhossain/Docs-client-site"><AiOutlineGithub/></a>
+           <a target='_blank' href="https://docs-client-site.vercel.app/"><HiOutlineExternalLink/></a>
+         </div>
+     </div>
+     <div className="col-lg-6">
+         <div className="projects mt-5">
+           <img src={project1} alt="" />
+         </div>
+    </div>
+    </div>
+        </div>
+         {/* project-3 */}
+         <div className="col-lg-12">
+          <div className="row div">
+          <div className="col-lg-6 ">
+         <div className="projects mt-5">
+           <img src={project1} alt="" />
+         </div>
+         </div>
+         <div className="col-lg-6 site ">
+        <p>Blogging Site</p>
+        <h3>Docy</h3>
+        <div className="info ">
+        A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, 
+        Package Control, Atom Package Manager, and npm.
+        </div>
+        <ul className='mt-4 d-flex justify-content-end tools'><li>VS Code</li>
+         <li> Sublime Text</li>
+         <li>Atom</li>
+         <li>iTerm2</li>
+         <li>Hyper</li></ul>
+         <div className="link-up">
+           <a target='_blank' href="https://github.com/thekawsarhossain/Docs-client-site"><AiOutlineGithub/></a>
+           <a target='_blank' href="https://docs-client-site.vercel.app/"><HiOutlineExternalLink/></a>
+         </div>
+         <div className="projects-btn">
+             <Link to='/projects'>
+             Learn more
+             </Link>
+            </div>
+         </div>
+          </div>
+        </div>
+       
        </div>
+       
      </div>
     </div>
   );
