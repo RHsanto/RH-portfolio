f7e83ba1ecf141660f2eb80996040ec894698132
RHsanto <rakibulsanto789@gmail.com>
2022-04-24 01:52:28 +0600

    add

--- src/App.js
+++ src/App.js
@@ -8,12 +8,42 @@ import Blog from './components/Blog/Blog';
 import Contact from './components/Contact/Contact';
 import AllProjects from './components/AllProjects/AllProjects';
 import MbProjects from './components/Projects/MbProjects';
-
+import { useEffect, useState } from 'react';
+import { CircleLoader } from "react-awesome-loaders";
+import './App.css'
 function App() {
+   const [loading , setLoading]= useState(false) ;
+
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },2000)
+  },[])
   return (
     <div>
+    {
+        loading ?
+       <div className="container">
+         <div className="row">
+           <div className="col-3 mx-auto">
+           <div >
+            
+      <CircleLoader
+       className='loading' 
+        meshColor={"#21E6C1"}
+        lightColor={"#E0E7FF"}
+        duration={1.5}
+        desktopSize={"90px"}
+        mobileSize={"64px"}
+      />
+       <h1 className='text-light'>Loading . . .</h1>
+      </div>
+           </div>
+         </div>
+       </div>
+        : 
       <Router>
-    
        <Navbar/>
        <Switch>
          <Route exact path ='/'>
@@ -39,6 +69,7 @@ function App() {
          </Route>
        </Switch>
      </Router>
+}
     </div>
   );
 }
